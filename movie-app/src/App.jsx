da8dee84b77a679f71039920feab69db8a55d5ff
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2025-08-19 21:14:44 +0100

    go

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import Search from './components/search.jsx'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  }
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try {
+       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+       const response = await fetch(endpoint, API_OPTIONS);
+
+       alert(response);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage(`value: 'Error fetching movies. Please try again later.'`);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  },[]);
+  
+  return (
+    <main>
+      <div className='pattern' />
+    
+      <div className='wrapper'>
+        <header>
+          <img src="./hero-img.png" alt='Hero Banner' />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+           
+        <section className='all-movies'>
+           <h2>All Movies</h2>
+
+            {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+        </section>
+       
+      </div>
+    </main>
+  )
+}
+
+export default App
